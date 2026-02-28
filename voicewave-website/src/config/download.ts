409e9568497cb34b40081489fae4280c543f6b06
rithwik1510 <rr7659@srmist.edu.in>
2026-02-28 17:22:38 +0530

    Fix Windows installer icon wiring and latest release download link

--- voicewave-website/src/config/download.ts
+++ voicewave-website/src/config/download.ts
@@ -1,2 +1,2 @@
 export const windowsDownloadUrl =
-  "https://github.com/rithwik1510/VoiceWave/releases/latest/download/VoiceWave.Local.Core_0.1.2_x64-setup.exe";
+  "https://github.com/rithwik1510/VoiceWave/releases/latest";
